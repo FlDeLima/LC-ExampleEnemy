a99bd848b84d4ce1620e478e280f7bbed0011372
Flule <florian.de_lima@hotmail.com>
2024-09-22 20:15:02 +0200

    refaire

--- Plugin/src/Plugin.cs
+++ Plugin/src/Plugin.cs
@@ -5,6 +5,7 @@ using LethalLib.Modules;
 using BepInEx.Logging;
 using System.IO;
 using ExampleEnemy.Configuration;
+using System.Collections.Generic;
 
 namespace ExampleEnemy {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
@@ -40,34 +41,33 @@ namespace ExampleEnemy {
             var ExampleEnemyTK = ModAssets.LoadAsset<TerminalKeyword>("ExampleEnemyTK");
 
             // Optionally, we can list which levels we want to add our enemy to, while also specifying the spawn weight for each.
-            /*
+            
             var ExampleEnemyLevelRarities = new Dictionary<Levels.LevelTypes, int> {
-                {Levels.LevelTypes.ExperimentationLevel, 10},
-                {Levels.LevelTypes.AssuranceLevel, 40},
-                {Levels.LevelTypes.VowLevel, 20},
-                {Levels.LevelTypes.OffenseLevel, 30},
-                {Levels.LevelTypes.MarchLevel, 20},
-                {Levels.LevelTypes.RendLevel, 50},
-                {Levels.LevelTypes.DineLevel, 25},
+                {Levels.LevelTypes.ExperimentationLevel, 10000},
+                //{Levels.LevelTypes.AssuranceLevel, 40},
+                //{Levels.LevelTypes.VowLevel, 20},
+                {Levels.LevelTypes.OffenseLevel, 100},
+                //{Levels.LevelTypes.MarchLevel, 20},
+                //{Levels.LevelTypes.RendLevel, 50},
+                {Levels.LevelTypes.DineLevel, 200},
                 // {Levels.LevelTypes.TitanLevel, 33},
-                // {Levels.LevelTypes.All, 30},     // Affects unset values, with lowest priority (gets overridden by Levels.LevelTypes.Modded)
-                {Levels.LevelTypes.Modded, 60},     // Affects values for modded moons that weren't specified
+                 {Levels.LevelTypes.All, 50},     // Affects unset values, with lowest priority (gets overridden by Levels.LevelTypes.Modded)
+                //{Levels.LevelTypes.Modded, 60},     // Affects values for modded moons that weren't specified
             };
             // We can also specify custom level rarities
             var ExampleEnemyCustomLevelRarities = new Dictionary<string, int> {
-                {"EGyptLevel", 50},
-                {"46 Infernis", 69},    // Either LLL or LE(C) name can be used, LethalLib will handle both
+                //{"EGyptLevel", 50},
+                //{"46 Infernis", 69},    // Either LLL or LE(C) name can be used, LethalLib will handle both
             };
-            */
 
             // Network Prefabs need to be registered. See https://docs-multiplayer.unity3d.com/netcode/current/basics/object-spawning/
             // LethalLib registers prefabs on GameNetworkManager.Start.
             NetworkPrefabs.RegisterNetworkPrefab(ExampleEnemy.enemyPrefab);
 
             // For different ways of registering your enemy, see https://github.com/EvaisaDev/LethalLib/blob/main/LethalLib/Modules/Enemies.cs
-            Enemies.RegisterEnemy(ExampleEnemy, BoundConfig.SpawnWeight.Value, Levels.LevelTypes.All, ExampleEnemyTN, ExampleEnemyTK);
+            //Enemies.RegisterEnemy(ExampleEnemy, BoundConfig.SpawnWeight.Value, Levels.LevelTypes.All, ExampleEnemyTN, ExampleEnemyTK);
             // For using our rarity tables, we can use the following:
-            // Enemies.RegisterEnemy(ExampleEnemy, ExampleEnemyLevelRarities, ExampleEnemyCustomLevelRarities, ExampleEnemyTN, ExampleEnemyTK);
+            Enemies.RegisterEnemy(ExampleEnemy, ExampleEnemyLevelRarities, ExampleEnemyCustomLevelRarities, ExampleEnemyTN, ExampleEnemyTK);
             
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
